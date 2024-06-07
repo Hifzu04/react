156153331d86c8c4388ba894d95b16ac8e2f5e59
Hifzu04 <hifzurbtech@gmail.com>
2024-06-07 10:33:09 +0530

    tailwindcss and props react

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -6,18 +6,18 @@ import Card from './components/Cards'
 
 function App() {
   const [count, setCount] = useState(0)
-  let myobj = {
-    name : "hrk" ,
-    age : 22 
-  }
+  // let myobj = {
+  //   name : "hrk" ,
+  //   age : 22 
+  // }
  
   return (
     <>
 
       <h1 className='text-sky-400 bg-zinc-700 rounded-md'>Tailwind test</h1>
       <h2>now props test</h2>
-      <Card firstPass="this is first" obj={myobj}/>
-      <Card/>
+      <Card username="micheal" btntext="click me" />
+      <Card username="thor" />
     
           </>
   )
