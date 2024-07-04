ec090bd5d8e4e9ccb5cac747f9356278d910c70e
Hifzu04 <hifzurbtech@gmail.com>
2024-07-04 12:18:34 +0530

    needed git component to form

--- 07RouterProject/src/main.jsx
+++ 07RouterProject/src/main.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element : <Home/>
+      },
+      {
+        path : "about",
+        element : <About/>
+      },
+      {
+        path : "contact",
+        element : <Contact/>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
