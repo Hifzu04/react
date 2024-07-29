e9057ae000037f5caf6b4a727f099e81d2ea2712
Hifzu04 <hifzurbtech@gmail.com>
2024-07-29 19:35:15 +0530

    context and their functionality done

--- 10TodoProject/src/App.jsx
+++ 10TodoProject/src/App.jsx
@@ -4,6 +4,21 @@ import TodoForm from './Components/TodoForm'
 import TodoItems from './Components/TodoItems'
 
 function App() {
+  const [Todos , setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now() , ...todo}] , ...prev)
+  }
+
+  const updateTodo = (id , todo) => {
+    setTodos((prev) => prev.map((eachprev) => (eachprev.id === id ? todo : eachprev)))
+  }
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevtodo) => prevtodo.id !== id))
+  }
+  const toggleTodo = (id) => {
+    setTodos((prev) => prev.map((prevtodo) => prevtodo.id === id ?
+    {...prevtodo , completed : !prevtodo.completed } : prevtodo))
+  } 
 
 
   return (
