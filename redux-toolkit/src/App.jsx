fcf2f08bfa0068c0be7ac6026c78d26273671977
Hifzu04 <hifzurbtech@gmail.com>
2024-08-27 20:10:02 +0530

    needs debugging

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,9 @@ function App() {
   return (
     <>
     <h1>learn about redux toolkit</h1>
-          </>
+    <AddTodo/>
+    <Todos/>
+   </>
   )
 }
 
