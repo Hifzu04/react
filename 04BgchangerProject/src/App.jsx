642bf90162f7c7c0eed7ed4d51444f6fea935119
Hifzu04 <hifzurbtech@gmail.com>
2025-03-25 17:25:59 +0530

    done

--- 04BgchangerProject/src/App.jsx
+++ 04BgchangerProject/src/App.jsx
@@ -1,35 +1,53 @@
-
-
-//used tailwindCSS in this project
-
-import { useState } from "react"
-
-
- 
+import { useState } from "react";
 
 function App() {
-  const [color, setcolor] = useState("#674645");    //read and write
+  const [color, setcolor] = useState("#970945");
 
-  return (
-    <>
-      <div className='width-full h-screen duration-1000' style={{ backgroundColor: color }}>
-        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-12'>
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 bg-yellow-50 rounded-3xl">
-            <button onClick={() => setcolor("red")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "red" }}>red</button>
-            <button onClick={() => setcolor("green")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "green" }}>green</button>
-            <button onClick={() => setcolor("grey")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "grey" }}>grey</button>
-            <button onClick={() => setcolor("blue")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "blue" }}>blue</button>
-            <button onClick={() => setcolor("orange")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "orange" }}>orange</button>
-            <button onClick={() => setcolor("indigo")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "indigo" }}>indigo</button>
-            <button onClick={() => setcolor("black")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "black" }}>black</button>
-            <button onClick={() => setcolor("purple")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "purple" }}>purple</button>
-            <button onClick={() => setcolor("pink")} className=' px-4 py-2 outline-none rounded-full shadow-sm text-white' style={{ backgroundColor: "pink" }}>pink</button>
-          </div>
+  return (                                                                             
+    <div className="w-full h-screen duration-1000" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 ">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 bg-yellow-50 rounded-3xl">
+          <button onClick={() => setcolor("red")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-red-500 hover:bg-red-600 transition-colors duration-300">
+            red
+          </button>
+          <button onClick={() => setcolor("green")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-green-500 hover:bg-green-600 transition-colors duration-300">
+            green
+          </button>
+          <button onClick={() => setcolor("gray")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-gray-500 hover:bg-gray-600 
+            transition-colors duration-300">
+            grey 
+          </button>
+          <button onClick={() => setcolor("blue")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-blue-500 hover:bg-blue-600 transition-colors duration-300">
+            blue
+          </button>
+          <button onClick={() => setcolor("orange")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-orange-500 hover:bg-orange-600 transition-colors duration-300">
+            orange
+          </button>
+          <button onClick={() => setcolor("indigo")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-indigo-500 hover:bg-indigo-600 transition-colors duration-300">
+            indigo
+          </button>
+          <button onClick={() => setcolor("black")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-black hover:bg-gray-800 transition-colors duration-300">
+            black
+          </button>
+          <button onClick={() => setcolor("purple")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-purple-500 hover:bg-purple-600 transition-colors duration-300">
+            purple
+          </button>
+          <button onClick={() => setcolor("pink")} 
+            className="px-4 py-2 rounded-full shadow-sm text-white bg-pink-500 hover:bg-pink-600 transition-colors duration-300">
+            pink
+          </button>
         </div>
       </div>
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
